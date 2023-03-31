e04fb80a9b1d90b9724c9eafd353751a823b3cef
Joseph Cappadona <josephcappadona27@gmail.com>
2023-03-31 19:00:13 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,41 @@
+const path = require('path');
+
 module.exports = {
-    entry: [
-        './src/index.jsx'
-    ],
+    entry: {
+        index: './js/index.jsx',
+        analysis: './js/analysis.jsx',
+    },
     module: {
         rules: [
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                use: ['babel-loader']
-            }
-        ]
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env',
+                                      '@babel/react',
+                                      {'plugins': ['@babel/plugin-transform-runtime']}],
+                        }
+                    },
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpe?g|gif)$/,
+                use: ['url-loader'],
+            },
+        ],
+    },
+    node: {
+        fs: "empty"
     },
     output: {
-        path: __dirname + '/static',
-        filename: 'bundle.js'
-    }
+        path: path.resolve(__dirname, 'static'),
+        filename: '[name].bundle.js',
+    },
 };
