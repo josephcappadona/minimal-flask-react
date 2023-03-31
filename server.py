8caec854e93ddc8035095835b26df41eb4e66176
Joseph Cappadona <josephcappadona27@gmail.com>
2023-03-31 19:02:36 -0400

    Update server.py

--- server.py
+++ server.py
@@ -10,5 +10,18 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/api/analysis', methods=['POST'])
+def api_analysis():
+    data = request.json
+
+    if 'value' not in data:
+        error_code = 401
+        ret = {'message': 'POST was unsuccessful'}
+    else:
+        error_code = 201
+        ret = {'message': 'POST was successful'}
+    return json.dumps(ret, cls=NpEncoder), error_code
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=os.environ.get('PORT', 3000), debug=True)
